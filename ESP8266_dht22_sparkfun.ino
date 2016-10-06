c134f425d17c6c5f5d89484c5d087a7b0ede193a
jpolton <jpolton@gmail.com>
2016-10-06 20:33:20 +0100

    Adding relay function
    Debugged GET posting to data.sparkfun.
    Added relay functionality.

--- ESP8266_dht22_sparkfun.ino
+++ ESP8266_dht22_sparkfun.ino
@@ -6,14 +6,15 @@
 //   BOARD: ESP8266 NodeMCU 1.0 (ESP-12E module)
 //FIRMWARE: Arduino IDE
 //     URL: https://data.sparkfun.com/streams/aG8bAlQybziD5Qa780yY
-            http://pcwww.liv.ac.uk/~jpolton/plots/esp8266_dht22_x2.html 
+//            http://pcwww.liv.ac.uk/~jpolton/plots/esp8266_dht22_x2.html 
 //          /Users/jeff/Public/liv.ac.uk/plots/esp8266_dht22_x2.html
 // HISTORY:
 // 0.1.00 initial version: http://hpclab.blogspot.co.uk/2015/06/esp8266-based-wifi-weather-monitoring.html
 // 0.1.01 Edit to accommodate two sensors.
-// 
-//  STATUS: WORKS. Though I've not actually tried in on the Vivarium..
+// 1.0.00 Tested
+// 2.0.00 Relay control added and tested. (Note that the LED on the fan seems to sometime rapidly flash when the fan is switched on.) 
 
+//  STATUS: WORKS. 
 /*
     NOTES:
     This sketch sends data via HTTP GET requests to data.sparkfun.com service.
@@ -24,21 +25,34 @@
     Posts data e.g.:
 	https://data.sparkfun.com/input/aG8bAlQybziD5Qa780yY?private_key=KEPqaDKYqzsawdVNE8AD&hum_bot=50.7&hum_top=27.1&temp_bot=16.3&temp_top=29.7
  */
-
 #include <ESP8266WiFi.h>
 #include "DHT.h"
-#define DHTPIN_bot D5     // D5 pin of ESP8266
-#define DHTPIN_top D3     // D3 pin of ESP8266
-const char* ssid     = ".....";
+/* Header file for storing private keys */
+#include "ESP8266_dht22_sparkfun_keys.h"
+/* Either include a header file (above) with these private keys or fill them in here.
+const char* ssid     = "....";
 const char* password = "....";
+const char* publicKey = "....";
+const char* privateKey = "....";
+ */
+
+#define DHTPIN_bot D2     // D2 pin of ESP8266
+#define DHTPIN_top D3     // D3 pin of ESP8266
+int fan = D7;              // D7 pin for relay to fan
+int heater = D6;           // D6 pin for relay to heater
 const char* host = "data.sparkfun.com";
-const char* publicKey = "aG8bAlQybziD5Qa780yY";
-const char* privateKey = "...";
+float Ttop_threshold = 35.0; // Top temperature to activate heater
+float Tbot_threshold = 28.0; // Bottom temperature to active fan
+
 DHT dht_bot(DHTPIN_bot, DHT22, 30); // 30 is for cpu clock of esp8266 80Mhz
 DHT dht_top(DHTPIN_top, DHT22, 30);
+
 void setup() {
   Serial.begin(115200);
-  dht.begin();
+  dht_bot.begin();
+  dht_top.begin();
+  pinMode(fan, OUTPUT);
+  pinMode(heater, OUTPUT);
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -55,10 +69,12 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
 void loop() {
   delay(5000);
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  /////////////////////////////////////////////////////////////////////////////
   float h_bot = dht_bot.readHumidity();
   float h_top = dht_top.readHumidity();
 
@@ -85,6 +101,30 @@ void loop() {
   Serial.print(t_top);
   Serial.print(" *C\t");
             
+
+  // Check temperatures and switch the relay on and off.
+  // NOTE: relay LOW = ON / HIGH = OFF
+  //////////////////////////////////////////////////////
+  if (t_bot < Tbot_threshold) {
+    digitalWrite(fan,LOW);
+    //Serial.print(digitalRead(fan), 1);
+  }
+  else {
+    digitalWrite(fan,HIGH);
+  }      
+  if (t_top < Ttop_threshold) {
+    digitalWrite(heater,LOW);
+    //Serial.print(digitalRead(heater), 1);
+  }
+  else {
+    digitalWrite(heater,HIGH);
+  }
+
+
+
+
+  // Now do web stuff
+  ///////////////////
   Serial.print("connecting to ");
   Serial.println(host);
   // Use WiFiClient class to create TCP connections
@@ -121,6 +161,11 @@ void loop() {
   }
   Serial.println();
   Serial.println("closing connection");
+
+
+
+
   delay(600000); // Send data every 10 minutes
+//  delay(30000); // Send data every 30s
 }
 
--- README.md
+++ README.md
@@ -3,23 +3,32 @@ ESP8266 management and control of vivarium temperature.
 
 Method:
 * The temperature and humidity data is measured on two DHT22 sensors using an ESP8266 flashed using the Arduino IDE.
+* A relay is switched depending on measured temperatures relative to a thresholds for turning on the heater and a fan. 
 * The data are sent using a PUT request to a data.sparkfun.com data stream.
 * Google charts javascript, embedded in an html file, load and plot the sparkfun data stream. 
 
+The pass keys for the wireless network and the private key for the data.sparkfun web page are stored in a separate file of the form:
+// ESP8266_dht22_sparkfun_keys.h
+
+const char* ssid     = "....";
+const char* password = "....";
+const char* publicKey = "aG8bAlQybziD5Qa780yY";
+const char* privateKey = "....";
+
 To-do:
-* Add private key to .ino file
-* Add network key to .ino file
 * Assess stability of code
-* Add relay control of fan and heater
 
 Wiring:
-#define DHTPIN_bot D5     // D5 pin of ESP8266
+#define DHTPIN_bot D2     // D2 pin of ESP8266
 #define DHTPIN_top D3     // D3 pin of ESP8266
+int fan = D7;              // D7 pin for relay to fan
+int heater = D6;           // D6 pin for relay to heater
+
+Note:
+Temperature is read at the top and bottom of the vivarium. The top temperature controls the heater. The lower temperature controls whether a fan should mix the warm air down.
 
 Plan:
-Inputs- temperature from DHT22 sensor x2.
-Output- Relay control of heater and fan.
-Temperature is read at the top and bottom of the vivarium. The top temperature controls the heater. The lower temperature controls whether a fan should mix the warm air down.
+*Add in LDR control of thresholds.
 
 This code development is managed on github from <script src="https://gist.github.com/jpolton/611681f366708f26a97cca3895066dcf.js"></script>.
 
